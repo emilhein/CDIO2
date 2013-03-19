86a7fe9e1dd3fc1104bb4da10992dcb0abc27def
Emil hein <Emil@10.16.101.188>
2013-03-19 08:17:33 +0100

    Signed-off-by: Emil hein <Emil@10.16.101.188>

--- src/controller/Server.java
+++ src/controller/Server.java
@@ -5,12 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 public class Server extends Thread {
 	
 	ServerSocket listener;
-	
+
 	//# New
 	
 	public Server(int port) throws IOException {
@@ -64,7 +62,6 @@ public class Server extends Thread {
 		
 		@Override
 		public void run() {
-			Pattern patternRM20 = Pattern.compile("^RM20 8 \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\"$");
 			try {
 				while (true) {
 					String line = reader.readLine();
@@ -72,42 +69,10 @@ public class Server extends Thread {
 						break;
 					}
 					System.out.println(" " + socket.getInetAddress().getHostAddress() + ": Received: " + line + ".");
-					Matcher matcherRM20 = patternRM20.matcher(line);
 					if (line.equals("S")) {
-					
-						// Retuner netto vægt.
-						writer.writeBytes("S S " + " kg"); //# TODO: Indsæt netto vægt ved +, bruger . som decimal tegn.
-
-					} else if (line.equals("T")) {
-					
-						// Sæt og retuner tara.
-						writer.writeBytes("T S " + " kg"); //# TODO: Indsæt ny tara vægt ved +, bruger . som decimal tegn.
-
-					} else if (line.equals("DW")) {
-					
-						// Fjern meddelsen fra displayet og vend tilbage til visning af netto vægt.
-						//# TODO: Fjern meddelse fra displayet.
-						writer.writeBytes("DW A");
-					
-					} else if (line.startsWith("D ") && line.length() > 2) {
-						
-						// Vis meddelse på displayet.
-						line.substring(2); //# TODO: Vis denne meddelse på displayet.
-						writer.writeBytes("D A");
-
-					} else if (matcherRM20.matches()) {
-
-						// Vis tre meddelser på displayet og retuner den indtastede værdi.
-						matcherRM20.group(1); //# TODO: Vis denne meddelse som den første på displayet.
-						matcherRM20.group(2); //# TODO: Vis denne meddelse som den anden på displayet.
-						matcherRM20.group(3); //# TODO: Vis denne meddelse som den tredje på displayet.
-						writer.writeBytes("RM20 A \"" + "\""); //# TODO: Retuner svar. Skal A være et B?
-						
+						writer.writeBytes("Svar...");
 					} else {
-						
-						// Retuner en fejlmeddelse.
-						writer.writeBytes("Error."); //# TODO: Send rigtig fejlkode.
-					
+						writer.writeBytes("Error.");
 					}
 				}
 			} catch (IOException e) {
